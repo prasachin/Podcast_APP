57dca9cb3a133a80fdeb51aacb161509e5d715ac
sachin <sachinp@iitbhilai.ac.in>
2025-01-26 01:47:18 +0530

    some update

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    ignoreBuildErrors: true
+    ignoreBuildErrors: true,
   },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'lovely-flamingo-139.convex.cloud'
+        protocol: "https",
+        hostname: "lovely-flamingo-139.convex.cloud",
       },
       {
-        protocol: 'https',
-        hostname: 'sleek-capybara-771.convex.cloud'
+        protocol: "https",
+        hostname: "sleek-capybara-771.convex.cloud",
       },
       {
-        protocol: 'https',
-        hostname: 'img.clerk.com'
+        protocol: "https",
+        hostname: "img.clerk.com",
       },
-    ]
-  }
+      {
+        protocol: "https",
+        hostname: "kindred-snake-236.convex.cloud",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
